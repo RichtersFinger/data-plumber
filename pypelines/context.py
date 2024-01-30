006d135a89480e775102b66962dfa0785cd1f299
RichtersFinger <steffen.finger.privat@gmail.com>
2024-01-30 18:42:59 +0100

    [context] implement minimal 'PipelineContext'

--- pypelines/context.py
+++ pypelines/context.py
@@ -4,6 +4,17 @@
 ...
 """
 
+from typing import TypeAlias
+from dataclasses import dataclass
 
+StageOut: TypeAlias = tuple[str, str, int]
+
+
+@dataclass
 class PipelineContext:
-    ...
+    """
+    Internal class providing a `Pipeline` execution-context for
+    `stage.StageRef` classes.
+    """
+
+    stages: list[StageOut]
