3d27ccfc10a2804fc84da13f25af97c25c100c76
RichtersFinger <steffen.finger.privat@gmail.com>
2024-01-30 23:05:13 +0100

    [package] update version for initial release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = \
         .split("\n")
 
 setup(
-    version="0.0.0",
+    version="0.1.0",
     name="pypelines",
     description="lightweight but versatile python-framework for multi-stage information processing",
     long_description=long_description,
